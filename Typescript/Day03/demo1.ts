62a37dc597a915919ca4e6204a4cfc77c79d15cf
The Kiran Academy <71135924+Javabykiran-Angular@users.noreply.github.com>
2023-04-17 15:55:30 +0530

    Day03

---
+++ Typescript/Day03/demo1.ts
@@ -0,0 +1,10 @@
+
+// console.log("Hello World....");
+// console.log('Welcome to Angular 12 Module....');
+// console.log(`
+//         -------- Hopes so u r Enjoying....
+// `);
+
+var a1,a2,a3;
+var a_1,a$1;
+
