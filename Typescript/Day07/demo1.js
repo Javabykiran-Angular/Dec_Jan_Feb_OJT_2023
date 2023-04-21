6354ebb6c3c798e2ebb81a1cb380f18beb8cd4b0
The Kiran Academy <71135924+Javabykiran-Angular@users.noreply.github.com>
2023-04-21 16:11:26 +0530

    Day07

---
+++ Typescript/Day07/demo1.js
@@ -0,0 +1,27 @@
+//array 
+// array is growable & shrinkable dnymically  
+// no fixed size
+// array is homogenous as well as hetrogenous 
+var a = [4, 5, 6, 7];
+var a1 = [];
+var a2 = [7, 8, 9, 5.4];
+var a3 = [2.5, 'Sumit', true];
+var a4 = [4, 'Sumit', 8.5];
+var str = ["Sumit", 'is', 'angular 12 '];
+var str1 = ["c", 'r', 'p'];
+// for(let i=0;i<a.length;i++){
+//     console.log(`
+//         a[${i}]=${a[i]}
+//     `)
+// }
+// console.log(a);
+// console.log(a.join(" "))
+// console.log(a.join("$"))
+// console.log(a.join())
+//foreach
+// a.forEach((myvalue,i,arr)=>{
+//     console.log("Myvalue is "+myvalue+" index is "+i+"  => "+arr);
+// });
+str1.forEach(function (kuchbhi) {
+    console.log(kuchbhi);
+});
---
+++ Typescript/Day07/demo1.ts
@@ -0,0 +1,44 @@
+//array 
+// array is growable & shrinkable dnymically  
+// no fixed size
+// array is homogenous as well as hetrogenous 
+
+let a:number[]=[4,5,6,7];
+let a1:number[]=[];
+let a2=[7,8,9,5.4];
+let a3:any[]=[2.5,'Sumit',true];
+let a4=[4,'Sumit',8.5];
+
+let str:string[]=["Sumit",'is','angular 12 ']
+let str1:string[]=["c",'r','p'];
+
+
+// for(let i=0;i<a.length;i++){
+
+//     console.log(`
+//         a[${i}]=${a[i]}
+//     `)
+
+// }
+
+// console.log(a);
+
+// console.log(a.join(" "))
+// console.log(a.join("$"))
+// console.log(a.join())
+
+//foreach
+
+// a.forEach((myvalue,i,arr)=>{
+//     console.log("Myvalue is "+myvalue+" index is "+i+"  => "+arr);
+// });
+
+
+str1.forEach((kuchbhi)=>{
+    console.log(kuchbhi)
+})
+
+
+
+
+
