32cb9d29685a9d35460a4712d068b5244e309e5f
The Kiran Academy <71135924+Javabykiran-Angular@users.noreply.github.com>
2023-06-05 16:20:19 +0530

    Day18

---
+++ Angular/Day18/page2/page2.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-page2',
+  templateUrl: './page2.component.html',
+  styleUrls: ['./page2.component.css']
+})
+export class Page2Component implements OnInit {
+
+  myid:number=0;
+  constructor(private route:ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.getdataFromUrl();
+  }
+
+  getdataFromUrl(){
+      this.route.paramMap
+      .subscribe((param:any)=>{
+        console.log(param.get("id"));
+
+        this.myid=+param.get("id");
+      })
+  }
+
+}
---
+++ Angular/Day18/queryparamter/queryparamter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-queryparamter',
+  templateUrl: './queryparamter.component.html',
+  styleUrls: ['./queryparamter.component.css']
+})
+export class QueryparamterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
